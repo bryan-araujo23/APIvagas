38345928e968d640e79b28d789e3ae398e9e11cd
Antônio Ryan Araújo Ramos <antonioryanaraujo@gmail.com>
2023-10-14 16:07:50 -0300

    Exibindo os registros por ID

--- api/views.py
+++ api/views.py
@@ -10,7 +10,7 @@ from rest_framework import status
 
 
 class VagaList(APIView):
-    def get(self, request):    
+    def get(self, request):           # Exibir todos registros do nosso banco de dados
         try:            
             lista_vagas = Vaga.objects.all()
             serializer = VagaSerializer(lista_vagas, many=True)
@@ -19,13 +19,31 @@ class VagaList(APIView):
             return JsonResponse({'mensagem': "Ocorreu um erro no servidor"},
             status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    def post(self, request):
+    def post(self, request):         # Insire um  registro no banco de dados
         try:
             serializer = VagaSerializer(data=request.data)
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception:
+            return JsonResponse({'mensagem': "Ocorreu um erro no servidor"},
+                                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+
+class VagaDetalhes(APIView):
+    def get(self, request, pk):
+        try:
+            if pk == "0":
+                return JsonResponse({'mensagem': "O ID deve ser maior que zero."},
+                                    status=status.HTTP_400_BAD_REQUEST)
+            vaga = Vaga.objects.get(pk=pk)
+            serializer = VagaSerializer(vaga)
+            return Response(serializer.data)
+        except Vaga.DoesNotExist:
+            return JsonResponse({'mensagem': "A vaga não existe"},
+                                status=status.HTTP_404_NOT_FOUND)
         except Exception:
             return JsonResponse({'mensagem': "Ocorreu um erro no servidor"},
                                 status=status.HTTP_500_INTERNAL_SERVER_ERROR)
