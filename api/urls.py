71cadcdd4d7bcb6057062c2501d8c36db0f358ab
Antônio Ryan Araújo Ramos <antonioryanaraujo@gmail.com>
2023-10-14 16:44:32 -0300

    Definindo as rotas para os métodos GET, PUT, DELETE

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import VagaList
+from .views import VagaList, VagaDetalhes
 
 urlpatterns = [
     path('vagas', VagaList.as_view()),
+    path('vagas/<int:pk>', VagaDetalhes.as_view()),
 ]
